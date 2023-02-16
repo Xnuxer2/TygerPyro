bf7f760d8906a07b404e65c26e540b79a8c7339a
Black <120400651+Xnuxer2@users.noreply.github.com>
2023-02-16 17:28:44 +0700

    Update __init__.py

--- tyger/database/__init__.py
+++ tyger/database/__init__.py
@@ -1,4 +1,14 @@
-import motor.motor_asyncio
+# Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
+#
+# This file is part of < https://github.com/DevsExpo/FridayUB > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/DevsExpo/blob/master/LICENSE >
+#
+# All rights reserved.
 
-from config import MONGO_URL
-cli = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
+import logging
+
+from tyger import mongo_client
+from config import *
+
+db_x = mongo_client["tyger"]
